ee074eed2babbb024b6d6d5a7f0f7ba09f8f4731
sebastianBaquero98 <s.baqueroa@uniandes.edu.co>
2022-03-16 19:27:24 -0500

    [Fix]: Intento de hacer respopnsivo

--- app.js
+++ app.js
@@ -45,124 +45,160 @@ function cargarOrderDetail(){
         const r = document.getElementById('rowTotals')
         r.remove();
     }
-    
-    const containerMain = document.getElementById('itemsMain');
-    const itemArea = document.getElementById('itemArea');
-    containerMain.innerHTML = '';
-    containerMain.id='itemsMain';
-    containerMain.classList.add('row' ,'justify-content-center', 'mt-3');
-    const titleProducts = document.getElementById('titleMain');
-    titleProducts.innerText = 'ORDER DETAIL';
-    titleProducts.style = 'font-weight:700; font-size: 30px;border-bottom: 1px solid #000';
-    
-    const table = document.createElement('table');
-    table.classList.add('table', 'table-striped');
-    const thead = document.createElement('thead');
-    const tr = document.createElement('tr');
-    const thItem = document.createElement('th');
-    const thQty = document.createElement('th');
-    const thDes = document.createElement('th');
-    const thPrice = document.createElement('th');
-    const thAmount = document.createElement('th');
-    const thModify = document.createElement('th');
-
-    thItem.innerText = 'Item';
-    thQty.innerText = 'Qty';
-    thDes.innerText = 'Description';
-    thPrice.innerText = 'Unit Price';
-    thAmount.innerText = 'Amount';
-    thModify.innerText = 'Modify';
-
-    tr.appendChild(thItem);
-    tr.appendChild(thQty);
-    tr.appendChild(thDes);
-    tr.appendChild(thPrice);
-    tr.appendChild(thAmount);
-    tr.appendChild(thModify);
-    thead.appendChild(tr);
-    table.appendChild(thead);
-
-    const tbody = document.createElement('tbody');
-    let sumaPedido = 0;
-    //console.log(orderDetail);
-    orderDetail.forEach((product,index) =>{
+    if(window.innerWidth >= 576){
+        const containerMain = document.getElementById('itemsMain');
+        const itemArea = document.getElementById('itemArea');
+        containerMain.innerHTML = '';
+        containerMain.id='itemsMain';
+        containerMain.classList.add('row' ,'justify-content-center', 'mt-3');
+        const titleProducts = document.getElementById('titleMain');
+        titleProducts.innerText = 'ORDER DETAIL';
+        titleProducts.style = 'font-weight:700; font-size: 30px;border-bottom: 1px solid #000';
+        
+        const table = document.createElement('table');
+        table.classList.add('table', 'table-striped');
+        const thead = document.createElement('thead');
         const tr = document.createElement('tr');
-
-        const item = document.createElement('td');
-        item.innerText = index;
-
-        const qty = document.createElement('td');
-        qty.innerText = product.amount;
-
-        const description = document.createElement('td');
-        description.innerText = product.description;
-
-        const price = document.createElement('td');
-        price.innerHTML = product.price;
-
-        const amount = document.createElement('td');
-        amount.innerText = product.amount * product.price;
-
-        const modifyA = document.createElement('td');
-        const buttonAdd = document.createElement('button');
-        buttonAdd.classList.add('btn');
-        buttonAdd.style = 'background-color: #FDCD55;color:white;margin-right:10px;'
-        buttonAdd.innerText = '+';
-        let strin = JSON.stringify(product);
-        buttonAdd.setAttribute('onClick',`javascript:addOne(${strin})`);
-        const buttonLess = document.createElement('button');
-        buttonLess.classList.add('btn');
-        buttonLess.style = 'background-color: #FDCD55;color:white;'
-        buttonLess.innerText = '-';
-        buttonLess.setAttribute('onClick',`javascript:lessOne(${strin})`);
-        const divBtns = document.createElement('div');
-        divBtns.classList.add('row')
-        divBtns.appendChild(buttonAdd);
-        divBtns.appendChild(buttonLess);
-        modifyA.appendChild(divBtns);
-       
-        tr.appendChild(item);
-        tr.appendChild(qty);
-        tr.appendChild(description);
-        tr.appendChild(price);
-        tr.appendChild(amount);
-        tr.appendChild(modifyA);
-        tbody.appendChild(tr);
-        sumaPedido+= (product.price * product.amount);
-    })
-    //console.log(tbody.innerHTML);
-    table.appendChild(tbody);
-    //console.log(table)
-    containerMain.appendChild(table);
+        const thItem = document.createElement('th');
+        const thQty = document.createElement('th');
+        const thDes = document.createElement('th');
+        const thPrice = document.createElement('th');
+        const thAmount = document.createElement('th');
+        const thModify = document.createElement('th');
     
-    // Resumen  pedido
-    const rowTotals = document.createElement('div');
-    rowTotals.id = 'rowTotals';
-    rowTotals.classList.add('row')
-    const colLeft = document.createElement('div');
-    colLeft.classList.add('col-9');
-    const priceTotals = document.createElement('p');
-    priceTotals.innerHTML = `<strong>Total: ${sumaPedido}</strong>`;
-    colLeft.appendChild(priceTotals);
-    rowTotals.appendChild(colLeft);
-    const colRight = document.createElement('div');
-    colRight.classList.add('col','float-end');
-    const buttonCancel = document.createElement('button');
-    buttonCancel.classList.add('btn','btn-danger');
-    buttonCancel.style = 'margin-right:5px'
-    buttonCancel.innerText = 'Cancel';
-    buttonCancel.setAttribute('data-bs-toggle',"modal" );
-    buttonCancel.setAttribute('data-bs-target',"#cancelationModal" );
-    const buttonConfirm = document.createElement('button');
-    buttonConfirm.classList.add('btn','btn-success');
-    buttonConfirm.innerText = 'Confirm Order';
-    buttonConfirm.setAttribute('onClick','javascript:confirmOrder()')
-    colRight.appendChild(buttonCancel);
-    colRight.appendChild(buttonConfirm);
-    rowTotals.appendChild(colRight);
-    itemArea.appendChild(rowTotals);
-
-
+        thItem.innerText = 'Item';
+        thQty.innerText = 'Qty';
+        thDes.innerText = 'Description';
+        thPrice.innerText = 'Unit Price';
+        thAmount.innerText = 'Amount';
+        thModify.innerText = 'Modify';
+    
+        tr.appendChild(thItem);
+        tr.appendChild(thQty);
+        tr.appendChild(thDes);
+        tr.appendChild(thPrice);
+        tr.appendChild(thAmount);
+        tr.appendChild(thModify);
+        thead.appendChild(tr);
+        table.appendChild(thead);
+    
+        const tbody = document.createElement('tbody');
+        let sumaPedido = 0;
+        //console.log(orderDetail);
+        orderDetail.forEach((product,index) =>{
+            const tr = document.createElement('tr');
+    
+            const item = document.createElement('td');
+            item.innerText = index;
+    
+            const qty = document.createElement('td');
+            qty.innerText = product.amount;
+    
+            const description = document.createElement('td');
+            description.innerText = product.description;
+    
+            const price = document.createElement('td');
+            price.innerHTML = product.price;
+    
+            const amount = document.createElement('td');
+            amount.innerText = product.amount * product.price;
+    
+            const modifyA = document.createElement('td');
+            const buttonAdd = document.createElement('button');
+            buttonAdd.classList.add('btn');
+            buttonAdd.style = 'background-color: #FDCD55;color:white;margin-right:10px;'
+            buttonAdd.innerText = '+';
+            let strin = JSON.stringify(product);
+            buttonAdd.setAttribute('onClick',`javascript:addOne(${strin})`);
+            const buttonLess = document.createElement('button');
+            buttonLess.classList.add('btn');
+            buttonLess.style = 'background-color: #FDCD55;color:white;'
+            buttonLess.innerText = '-';
+            buttonLess.setAttribute('onClick',`javascript:lessOne(${strin})`);
+            const divBtns = document.createElement('div');
+            divBtns.classList.add('row')
+            divBtns.appendChild(buttonAdd);
+            divBtns.appendChild(buttonLess);
+            modifyA.appendChild(divBtns);
+           
+            tr.appendChild(item);
+            tr.appendChild(qty);
+            tr.appendChild(description);
+            tr.appendChild(price);
+            tr.appendChild(amount);
+            tr.appendChild(modifyA);
+            tbody.appendChild(tr);
+            sumaPedido+= (product.price * product.amount);
+        })
+        //console.log(tbody.innerHTML);
+        table.appendChild(tbody);
+        //console.log(table)
+        containerMain.appendChild(table);
+        
+        // Resumen  pedido
+        const rowTotals = document.createElement('div');
+        rowTotals.id = 'rowTotals';
+        rowTotals.classList.add('row')
+        const colLeft = document.createElement('div');
+        colLeft.classList.add('col-9');
+        const priceTotals = document.createElement('p');
+        priceTotals.innerHTML = `<strong>Total: ${sumaPedido}</strong>`;
+        colLeft.appendChild(priceTotals);
+        rowTotals.appendChild(colLeft);
+        const colRight = document.createElement('div');
+        colRight.classList.add('col','float-end');
+        const buttonCancel = document.createElement('button');
+        buttonCancel.classList.add('btn','btn-danger');
+        buttonCancel.style = 'margin-right:5px'
+        buttonCancel.innerText = 'Cancel';
+        buttonCancel.setAttribute('data-bs-toggle',"modal" );
+        buttonCancel.setAttribute('data-bs-target',"#cancelationModal" );
+        const buttonConfirm = document.createElement('button');
+        buttonConfirm.classList.add('btn','btn-success');
+        buttonConfirm.innerText = 'Confirm Order';
+        buttonConfirm.setAttribute('onClick','javascript:confirmOrder()')
+        colRight.appendChild(buttonCancel);
+        colRight.appendChild(buttonConfirm);
+        rowTotals.appendChild(colRight);
+        itemArea.appendChild(rowTotals);
+    
+    
+    }else{
+        const todoMobil = document.getElementById('mobileS');
+        const rowCard = document.createElement('div');
+        rowCard.classList.add('row', 'd-flex', 'justify-content-center')
+        console.log('Entro aca');
+        const card = document.createElement('div');
+        card.classList.add('card', 'boder', 'border-dark');
+        card.style = 'background-color: rgb(196,196,196,.34); width:376px; margin-top: 10px';
+        const cardBody = document.createElement('div');
+        cardBody.classList.add('card-body');
+        const containerr = document.createElement('div');
+        containerr.classList.add('container');
+        orderDetail.forEach(product=>{
+            const row = document.createElement('div');
+            row.classList.add('row');
+            const col1 = document.createElement('div');
+            col1.classList.add('col-1');
+            col1.innerText = product.amount;
+            const col2 = document.createElement('div');
+            col2.classList.add('col-6');
+            col2.innerText = product.name;
+            const col3 = document.createElement('div');
+            col3.classList.add('col-4');
+            col3.innerHTML = '<button style="background-color: #FDCD55;color:white;margin-right:10px;">+</button><button style="background-color: #FDCD55;color:white;">-</button>';
+            row.appendChild(col1);
+            row.appendChild(col2);
+            row.appendChild(col3);
+            containerr.appendChild(row);
+        })
+        cardBody.appendChild(containerr);
+        card.appendChild(cardBody);
+        rowCard.appendChild(card);
+        todoMobil.appendChild(rowCard);
+    }
+    
+    
 }
 function clearProducts(){
     //console.log('Entro al clear');
